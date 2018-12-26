eb79110e44484797cd2a6b9f27ecad2bb14f0978
hikaricp <jiyongguang0723@gmail.com>
2018-12-26 23:41:23 +0800

    希尔排序的优化

--- src/Sort/temp/ShellSort.java
+++ src/Sort/temp/ShellSort.java
@@ -15,16 +15,13 @@ public class ShellSort {
 
         for (int step = len / 2; step > 0; step /= 2)// 从len/2到1递减增量来排序
             for (int i = step; i < len; i++) {// 第一个拿元素比较的位置，默认0已经在手中w
-                int j = i - step;
+                int j;
                 int temp = arr[i];
 
                 // 找temp的合适位置
-                for (; j >= 0; j -= step)// 根据增量向前递减寻找前驱元素进行比较
-                    if (arr[j] > temp)
-                        arr[j + step] = arr[j];
-                    else
-                        break;
-                arr[j + step] = temp;
+                for (j = i; j >= 0 && arr[j - step] > temp; j -= step)
+                    arr[j] = arr[j - step];
+                arr[j] = temp;
             }
     }
 }
