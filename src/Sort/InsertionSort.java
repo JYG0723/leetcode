354b5106a82192b294f06e0611bb238215b0e854
hikaricp <jiyongguang0723@gmail.com>
2018-12-26 22:49:35 +0800

    插入排序的优化

--- src/Sort/InsertionSort.java
+++ src/Sort/InsertionSort.java
@@ -8,24 +8,16 @@ package Sort;
 public class InsertionSort {
 
     private static int[] insertionSort(int[] arr) {
-        int len = arr.length;
-        if (len <= 1) {
-            return arr;
-        }
 
-        // 像有序数据队中插入元素 并使原有序队继续有序
-        for (int i = 1; i < len; i++) {// 比较次数，从数组下标为1开始
-            int temp = arr[i];
-            int j = i - 1;
+        for (int i = 1; i < arr.length; i++) // 比较趟数
 
-            for (; j >= 0; j--) {
-                if (arr[j] > temp) // 切户忘记与arr[i]比较的是有序递增队列。
-                    arr[j + 1] = arr[j]; // 元素后移
-                else
+            for (int j = i; j > 0; j--)// 和前驱位置比较大小
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1]; // 元素后移
+                    arr[j - 1] = temp;
+                } else
                     break; // 数组前面元素不再含有比该值大的元素，此次比较结束
-            }
-            arr[j + 1] = temp;
-        }
         return arr;
     }
 
